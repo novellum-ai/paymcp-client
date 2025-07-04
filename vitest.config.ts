308d5f61f91747a16b73b593b272d633224afd4a
bdj <bdj@pr0xy.co>
2025-07-04 09:21:21 -0700

    Only run node tests under vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,6 +4,8 @@ export default defineConfig({
   test: {
     environment: 'node',
     globals: true,
+    include: ['src/**/*.test.ts', 'src/**/*.test.tsx'],
+    exclude: ['**/*.expo.test.ts', '**/*.expo.test.tsx', 'node_modules/**', 'dist/**'],
   },
   define: {
     global: 'globalThis',
