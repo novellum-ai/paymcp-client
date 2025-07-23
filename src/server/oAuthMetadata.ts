6c5dce205df3d71ece1d622ae6c2019eac3fc9a9
bdj <bdj@pr0xy.co>
2025-07-23 15:41:38 -0700

    Logging/handling for oauth metadata

--- src/server/oAuthMetadata.ts
+++ src/server/oAuthMetadata.ts
@@ -15,28 +15,32 @@ export function sendOAuthMetadata(res: ServerResponse, metadata: oauth.Authoriza
 
 export async function getOAuthMetadata(config: PayMcpConfig, requestUrl: URL): Promise<oauth.AuthorizationServer | null> {
   if (isOAuthMetadataRequest(config, requestUrl)) {
-    const authServer = await config.oAuthClient.authorizationServerFromUrl(new URL(config.server));
+    try {
+      const authServer = await config.oAuthClient.authorizationServerFromUrl(new URL(config.server));
 
-    return {
-      issuer: config.server,
-      authorization_endpoint: authServer.authorization_endpoint,
-      response_types_supported: authServer.response_types_supported,
-      grant_types_supported: authServer.grant_types_supported,
-      token_endpoint: authServer.token_endpoint,
-      token_endpoint_auth_methods_supported: authServer.token_endpoint_auth_methods_supported,
-      registration_endpoint: authServer.registration_endpoint,
-      revocation_endpoint: authServer.revocation_endpoint,
-      introspection_endpoint: authServer.introspection_endpoint,
-      introspection_endpoint_auth_methods_supported: authServer.introspection_endpoint_auth_methods_supported,
-      code_challenge_methods_supported: authServer.code_challenge_methods_supported,
-      scopes_supported: authServer.scopes_supported
-    };
+      return {
+        issuer: config.server,
+        authorization_endpoint: authServer.authorization_endpoint,
+        response_types_supported: authServer.response_types_supported,
+        grant_types_supported: authServer.grant_types_supported,
+        token_endpoint: authServer.token_endpoint,
+        token_endpoint_auth_methods_supported: authServer.token_endpoint_auth_methods_supported,
+        registration_endpoint: authServer.registration_endpoint,
+        revocation_endpoint: authServer.revocation_endpoint,
+        introspection_endpoint: authServer.introspection_endpoint,
+        introspection_endpoint_auth_methods_supported: authServer.introspection_endpoint_auth_methods_supported,
+        code_challenge_methods_supported: authServer.code_challenge_methods_supported,
+        scopes_supported: authServer.scopes_supported
+      };
+    } catch (error) {
+      config.logger.error(`Error fetching authorization server configuration from ${config.server}: ${error}`);
+      throw error;
+    }
   }
   return null;
 }
 
 function isOAuthMetadataRequest(config: PayMcpConfig, requestUrl: URL): boolean {
-  config.logger.debug(`Checking if ${requestUrl.toString()} is a OAuth metadata request`);
   const path = getPath(requestUrl).replace(/\/$/, '');
   return path === '/.well-known/oauth-authorization-server';
 }
