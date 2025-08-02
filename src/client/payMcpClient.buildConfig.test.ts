9f05115ce41c1ae42de18052735553c7fbe8b4f7
Rob Di Marco <robdimarco@pr0xy.co>
2025-08-01 23:01:41 -0400

    Remove unused code

--- src/client/payMcpClient.buildConfig.test.ts
+++ src/client/payMcpClient.buildConfig.test.ts
@@ -1,12 +1,5 @@
-import { SqliteOAuthDb } from '../common/oAuthDb';
-import { OAuthAuthenticationRequiredError } from './oAuth.js';
 import { describe, it, expect, vi } from 'vitest';
-import fetchMock from 'fetch-mock';
-import { mockResourceServer, mockAuthorizationServer } from './clientTestHelpers.js';
-import { PayMcpFetcher } from './payMcpFetcher.js';
-import { OAuthDb, FetchLike, DEFAULT_AUTHORIZATION_SERVER } from '../common/types.js';
-import { PaymentMaker } from './types.js';
-import { buildClientConfig, payMcpClient } from './payMcpClient.js';
+import { buildClientConfig } from './payMcpClient.js';
 
 describe('buildConfig', () => {
   it('should use fetchFn for oAuthChannelFetch if the former is provided and not the latter', () => {
